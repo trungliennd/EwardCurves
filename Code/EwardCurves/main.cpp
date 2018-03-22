5e144e646c1d4ea3dcea72b79e82be367a30eaeb
trungliennd <20134145@student.hust.edu.vn>
2018-03-22 22:44:40 +0700

    update

--- Code/EwardCurves/main.cpp
+++ Code/EwardCurves/main.cpp
@@ -21,13 +21,17 @@ int main() {
     char* file1 = (char*)"Alice.pub";
     char* file2 = (char*)"Bob";
     char* file3 = (char*)"Bob.pub";
-   // createPublicKeyAndSecretKey(file, file1);
+  //  createPublicKeyAndSecretKey(file, file1);
    // createPublicKeyAndSecretKey(file2, file3);
-    initCurveTwistEwards25519();
-    loadKey(file,file3);
-    crypto_scalarmult(sharesKey25519, secretKey25519, publicKey25519);
-  //  printKey(sharesKey25519, 32);
-   //    loadKey(file2, file1);
+  //  initCurveTwistEwards25519();
+  //  printf("\n\nLoad1");
+  //  loadKey(file,file3); //pub: 47541145414185450203032252793825292412973981143890313845229093811630295353916
+  //  crypto_scalarmult(sharesKey25519, secretKey25519, publicKey25519);
+ //   printKey(sharesKey25519, 32);
+  //  printf("\n\nload2");
+   // loadKey(file2, file1);
    // crypto_scalarmult(sharesKey25519, secretKey25519, publicKey25519);
   //  printKey(sharesKey25519, 32);
+
+
 }
